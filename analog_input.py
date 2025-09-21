26642affd2807b0ac15186b320c21bef4810468d
Matt Martin <mattmartin1891@gmail.com>
2025-09-21 05:22:24 -0400

    adding analog input

---
+++ analog_input.py
@@ -0,0 +1,26 @@
+
+
+class AnalogInput:
+    def __init__(self, name, euMin, euMax, rawMin, rawMax, raw):
+        self.name = name
+        self.euMin = euMin
+        self.euMax = euMax
+        self.rawMin = rawMin
+        self.rawMax = rawMax
+        self.raw = raw
+
+    def scaling(self):
+        scaled = ((self.euMax-self.euMin)*(self.raw-self.rawMin)/ (self.rawMax-self.rawMin))
+        return scaled
+        
+
+    def __str__(self):
+        return f"{self.name}({self.age})"
+    
+
+
+sensor = AnalogInput("TIT", 0, 10, 4, 20, 12)
+
+print(sensor.scaling())
+print(sensor.name)
+
